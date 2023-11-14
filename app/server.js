fcfbaeea44f97097da9d1e0c943336442f8d8bbb
Chadd Brenner <chaddbrenner@chadds-mbp.elon.edu>
2023-11-14 13:39:49 -0500

    updates

--- app/server.js
+++ app/server.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-// const { auth } = require('express-openid-connect');
-// const { requiresAuth } = require('express-openid-connect');
 const { expressjwt: jwt } = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
-// const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 
 
 require('dotenv').config();
 
-
-
 const app = express();
-
-// const checkJwt = jwt({secret: process.env.AUTH0_SECRET, algorithms: ['RS256'], audience: 'http://localhost:3000', issuer: process.env.AUTH0_ISSUER_BASE_URL});
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -27,34 +20,6 @@ const checkJwt = jwt({
   algorithms: ['RS256']
 });
 
-
-var corsOptions = {
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-};
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.AUTH0_SECRET,
-  baseURL: process.env.AUTH0_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-};
-
-// Authorization middleware. When used, the Access Token must
-// exist and be verified against the Auth0 JSON Web Key Set.
-// const checkJwt = auth({
-  // audience: 'http://localhost:3000',
-  // issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  // algorithms: ['RS256'],
-  // tokenSigningAlgorithm: "RS256",
-  // secret: process.env.AUTH0_SECRET,
-// });
-
-
-// app.use(cors({origin: '*'}));
 app.use(cors());
 
 // parse requests of content-type - application/json
@@ -63,43 +28,8 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// Auth0
-// app.use(auth(config));
 
-// // simple route
-// app.get("/test", (req, res) => {
-//   console.log(req.oidc.accessToken);
-//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-//   // res.json({ message: "Welcome to bezkoder application." });
-// });
-
-// app.get('/profile', requiresAuth(), (req, res) => {
-//   console.log("here");
-//   res.send(JSON.stringify(req.oidc.user));
-// });
-
-// app.get(
-//   "/protected",
-//   jwt({ secret: process.env.AUTH0_SECRET, algorithms: ['HS256']}),
-//   function (req, res) {
-//     console.log(req.auth);
-//     console.log("here")
-//     // if (!req.auth.admin) return res.sendStatus(401);
-//     res.sendStatus(200);
-//   });
-//   // (req, res) => {
-//   //   console.log(req.headers);
-//   //   res.json({abc: 'You are authenticated'});
-    
-//   // }
-// // );
-
-// app.get("/test", checkJwt, (req, res) => {
-//   console.log(req.auth);
-//   res.json({abc: 'You are authenticated'});
-// });
-
-app.get("/test", checkJwt, (req, res) => {
+app.get("/test", checkJwt, async (req, res) => {
   console.log(req.auth);
   res.json({abc: 'You are authenticated'});
 });
@@ -108,7 +38,18 @@ require("./routes/restaurant.routes.js")(app);
 require("./routes/station.routes.js")(app);
 require("./routes/meal.routes.js")(app);
 require("./routes/food.routes.js")(app);
-require("./routes/user.routes.js")(app);
+require("./routes/user.routes.js")(app, checkJwt);
+
+// Handle unauthorized requests so that the sever does not send out a stack trace
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+      res.status(401).send('Invalid token, or no token supplied');
+  } else {
+      // Log the error for server monitoring
+      console.error(err);
+      res.status(500).send('An internal server error occurred');
+  }
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 9000;
--- public2/js/restaurant.js
+++ public2/js/restaurant.js
@@ -3,61 +3,103 @@ const urlParams = new URLSearchParams(queryString);
 const restaurantId = urlParams.get('id');
 
 
-let auth0 = null;
-
-const configureClient = async () => {
-  auth0 = await createAuth0Client({
+auth0.createAuth0Client({
     domain: "dev-ilbuu4john1p274i.us.auth0.com",
-    client_id: "Q5vXLbd5NoMtXFAiPjjXHuC0POe4Cqdf",
-  });
-};
-
-window.onload = async () => {
-    await configureClient();
-    const isAuthenticated = await auth0.isAuthenticated();
-    console.log(isAuthenticated);
-    // updateUI();
-    const query = window.location.search;
-    if (query.includes("code=") && query.includes("state=")) {
-      // Process the login state
-      await auth0.handleRedirectCallback();
-      updateUI();
-      // Use replaceState to redirect the user away and remove the querystring parameters
+    clientId: "Q5vXLbd5NoMtXFAiPjjXHuC0POe4Cqdf",
+    authorizationParams: {
+        redirect_uri: window.location.href,
+        audience: 'http://localhost:3000', 
+        scope: 'openid profile email',
+    }
+  }).then(async (auth0Client) => {
+    // Assumes a button with id "login" in the DOM
+    const loginButton = document.getElementById("login");
+  
+    loginButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      auth0Client.loginWithRedirect();
+    });
+  
+    if (location.search.includes("state=") && 
+        (location.search.includes("code=") || 
+        location.search.includes("error="))) {
+      await auth0Client.handleRedirectCallback();
       window.history.replaceState({}, document.title, "/");
     }
-};
-
-console.log(window.location.origin);
-const login = async () => {
-    await auth0.loginWithRedirect({
-      redirect_uri: window.location.origin
-    });
-  };
   
-  const logout = () => {
-    auth0.logout({
-      returnTo: window.location.origin
+    // Assumes a button with id "logout" in the DOM
+    const logoutButton = document.getElementById("logout");
+  
+    logoutButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      auth0Client.logout();
     });
-};
+  
+    const isAuthenticated = await auth0Client.isAuthenticated();
+    console.log(isAuthenticated);
+    // const userProfile = await auth0Client.getUser();
+  
+    // // Assumes an element with id "profile" in the DOM
+    // const profileElement = document.getElementById("profile");
+  
+    // if (isAuthenticated) {
+    //   profileElement.style.display = "block";
+    //   profileElement.innerHTML = `
+    //           <p>${userProfile.name}</p>
+    //           <img src="${userProfile.picture}" />
+    //         `;
+    // } else {
+    //   profileElement.style.display = "none";
+    // }
 
 
+    //with async/await
+    // document.getElementById('call-api').addEventListener('click', async () => {
+
+    //     // const isAuthenticated = await auth0Client.isAuthenticated();
+    //     // if (!isAuthenticated) {
+    //     //     return alert('User is not authenticated');
+    //     // }
+    //     console.log('clicked');
+    //     const accessToken = await auth0Client.getTokenSilently();
+    //     const result = await fetch('http://localhost:3000/api/user', {
+    //     method: 'GET',
+    //     headers: {
+    //         Authorization: `Bearer ${accessToken}`,
+    //         'Content-Type': 'application/json',
+    //     }
+    //     });
+    //     const data = await result.json();
+    // });
+});
 
 async function getFoods(restaurantId) {
     let meals = await fetch("http://localhost:3000/api/meals/restaurant/" + restaurantId);
+    if (meals.status == 404) {
+        return [];
+    }
+
     meals = await meals.json();
 
     for (meal of meals) {
         let stations = await fetch("http://localhost:3000/api/stations/meal/" + meal.id);
+        if (stations.status == 404) {
+            continue;
+        }
+
         stations = await stations.json();
 
         for (station of stations) {
             let foods = await fetch("http://localhost:3000/api/foods/station/" + station.id);
+            if (foods.status == 404) {
+                continue;
+            }
+
             foods = await foods.json();
             station.foods = foods;
         }
         meal.stations = stations;
     }
-    console.log(meals);
     return meals;
 }
 
@@ -109,6 +151,10 @@ function openTab(mealName) {
     }
 }
 
+function openFood(food) {
+    console.log(food);
+}
+
 async function displayFoods(restaurantId) {
     let foods = await getFoods(restaurantId);
 
@@ -118,7 +164,6 @@ async function displayFoods(restaurantId) {
         mealButton.classList.add("tab-button");
         mealButton.innerText = meal.name;
         mealButton.setAttribute("onclick", "openTab('" + meal.name.replace(" ", "") + "')");
-        console.log(meal);
         if (isActive(meal)) {
             mealButton.classList.add("active");
         }
@@ -126,59 +171,73 @@ async function displayFoods(restaurantId) {
         mealSelector.appendChild(mealButton);
 
         let stationList = document.getElementById("station-list");
+        if (meal.stations) {
+            if (meal.stations) {
+                for (station of meal.stations) {
+                    let stationDiv = document.createElement("div");
+                    stationDiv.classList.add("station");
+                    stationDiv.classList.add(meal.name.replace(" ", ""));
+                    if (!isActive(meal)) {
+                        stationDiv.classList.add("hidden");
+                    }
+                    stationDiv.setAttribute("id", station.name);
+        
+                    let stationName = document.createElement("h2");
+                    stationName.innerText = station.name;
+                    
+                    stationDiv.appendChild(stationName);
+        
+                    let foodList = document.createElement("ul");
+                    foodList.classList.add("food-list");
+                    
+                    for (food of station.foods) {
+                        let foodItem = document.createElement("li");
+                        let foodLink = document.createElement("a");
+                        foodLink.setAttribute("href", "#");
+                        foodLink.innerText = food.short_name;
+                        foodLink.addEventListener("click", (e) => {
+                            console.log("clicked");
+                            console.log(e.target);
+                            openFood(food);
+                        });
+                        foodLink.setAttribute("data-food-id", food.id);
+                        foodLink.classList.add("food-link");
+                        foodItem.appendChild(foodLink);
 
-        for (station of meal.stations) {
-            let stationDiv = document.createElement("div");
-            stationDiv.classList.add("station");
-            stationDiv.classList.add(meal.name.replace(" ", ""));
-            if (!isActive(meal)) {
-                stationDiv.classList.add("hidden");
+                        foodItem.classList.add("food-item");
+                        // foodItem.innerText = food.short_name;
+        
+                        
+                        if (food.gluten_free) {
+                            let glutenFree = document.createElement("img");
+                            glutenFree.setAttribute("src", "images/glutenFree2.png");
+                            glutenFree.classList.add("gluten-free");
+                            foodItem.appendChild(glutenFree);
+                        }
+        
+                        if (food.vegetarian) {
+                            let vegetarian = document.createElement("img");
+                            vegetarian.setAttribute("src", "images/vegetarian2.png");
+                            vegetarian.classList.add("vegetarian");
+                            foodItem.appendChild(vegetarian);
+                        }
+        
+                        if (food.vegan) {
+                            let vegan = document.createElement("img");
+                            vegan.setAttribute("src", "images/vegan2.png");
+                            vegan.classList.add("vegan");
+                            foodItem.appendChild(vegan);
+                        }
+        
+                        foodList.appendChild(foodItem);
+                    }
+        
+                    stationDiv.appendChild(foodList);
+        
+        
+                    stationList.appendChild(stationDiv);
+                }
             }
-            stationDiv.setAttribute("id", station.name);
-
-            let stationName = document.createElement("h2");
-            stationName.innerText = station.name;
-
-            stationDiv.appendChild(stationName);
-
-            let foodList = document.createElement("ul");
-            foodList.classList.add("food-list");
-            
-            for (food of station.foods) {
-                let foodItem = document.createElement("li");
-                foodItem.classList.add("food-item");
-                foodItem.innerText = food.short_name;
-
-                
-                if (food.gluten_free) {
-                    let glutenFree = document.createElement("img");
-                    glutenFree.setAttribute("src", "images/glutenFree2.png");
-                    glutenFree.classList.add("gluten-free");
-                    foodItem.appendChild(glutenFree);
-                }
-
-                if (food.vegetarian) {
-                    let vegetarian = document.createElement("img");
-                    vegetarian.setAttribute("src", "images/vegetarian2.png");
-                    vegetarian.classList.add("vegetarian");
-                    foodItem.appendChild(vegetarian);
-                }
-
-                if (food.vegan) {
-                    let vegan = document.createElement("img");
-                    vegan.setAttribute("src", "images/vegan2.png");
-                    vegan.classList.add("vegan");
-                    foodItem.appendChild(vegan);
-                }
-
-                foodList.appendChild(foodItem);
-            }
-
-            stationDiv.appendChild(foodList);
-
-
-            stationList.appendChild(stationDiv);
-
         }
     }
 
@@ -187,5 +246,17 @@ async function displayFoods(restaurantId) {
     firstMeal.click();
     
 }
+
+const foodItems = document.getElementsByClassName("food-item");
+for (foodItem of foodItems) {
+    foodItem.addEventListener("click", (e) => {
+        console.log("clicked");
+        console.log(e.target);
+    });
+}
+// foodItems.addEventListener("click", (e) => {
+//     console.log("clicked");
+//     console.log(e.target);
+// });
 
 displayFoods(restaurantId);
